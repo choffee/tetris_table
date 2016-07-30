c9a9841a7808857312d20a95a5f28d7f6dd46d64
John Cooper <john@choffee.co.uk>
2016-07-30 19:08:28 +0100

    Set hostname of pi

--- pi_setup.sh
+++ pi_setup.sh
@@ -4,7 +4,7 @@
 # Should be able to run this over and over.
 #
 
-grep "ARM" /proc/cpuinfo >/dev/null || echo "no pi?" && exit 1
+grep "ARM" /proc/cpuinfo >/dev/null || (echo "no pi?" && exit 1)
 
 add_line() {
     local file=$1
@@ -12,14 +12,14 @@ add_line() {
     grep "^${line}" "${file}" >/dev/null || echo "${line}" >> "${file}"
 }
 
+echo "Installing packages"
 apt-get install python3-smbus python3-spidev i2c-tools python3-pygame
 
-# Enable spi bus
-
+echo "Enabling spi bus"
 # Is there a module also?
 add_line /boot/config.txt "dtparam=spi=on"
 
-# Enable I2C bus
+echo "Enabling i2c bus"
 # Automaticaly load the module
 add_line /etc/modules "i2c-dev"
 add_line /etc/modules "i2c-bmc2708"
@@ -27,8 +27,13 @@ add_line /etc/modules "i2c-bmc2708"
 add_line /boot/config.txt "dtparam=i2c1=on"
 add_line /boot/config.txt "dtparam=i2c_arm=on"
 
+echo "Adding pi users to groups"
 adduser pi i2c
 adduser pi spi
+
+echo "Setting hostname"
+echo tetristable >/etc/hostname
+sed -i "s/127.0.1.1.*/127.0.1.1\ttetristable/g" /etc/hosts
 
 sync
 
