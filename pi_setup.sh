6fd3708e4727de5269b86b5d4b65667e4db6afe6
John Cooper <john@choffee.co.uk>
2016-07-30 16:41:26 +0100

    Added pi setup script

---
+++ pi_setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Setup a pi from scratch to run the game.
+# Should be able to run this over and over.
+#
+
+grep "ARM" /proc/cpuinfo >/dev/null || echo "no pi?" && exit 1
+
+add_line() {
+    local file=$1
+    local line=$2
+    grep "^${line}" "${file}" >/dev/null || echo "${line}" >> "${file}"
+}
+
+apt-get install python3-smbus python3-spidev i2c-tools python3-pygame
+
+# Enable spi bus
+
+# Is there a module also?
+add_line /boot/config.txt "dtparam=spi=on"
+
+# Enable I2C bus
+# Automaticaly load the module
+add_line /etc/modules "i2c-dev"
+add_line /etc/modules "i2c-bmc2708"
+# Now set the boot config to turn it on.
+add_line /boot/config.txt "dtparam=i2c1=on"
+add_line /boot/config.txt "dtparam=i2c_arm=on"
+
+adduser pi i2c
+adduser pi spi
+
+sync
+
+echo "You might want to reboot now :)"
+
+
