3af257dfa5a40cc1dafa40f45eabca7cd774454e
John Cooper <john@choffee.co.uk>
2016-08-01 18:39:07 +0000

    Added some basic intrupt/callback to buttons

--- buttons.py
+++ buttons.py
@@ -3,13 +3,26 @@
 from Adafruit_GPIO import MCP230xx as mcs
 from Adafruit_GPIO import GPIO
 
+interupt_pin = 18
+i2c_address = 0x20
+
 class Buttons(object):
 
     def __init__(self):
-        self.buttons = mcs.MCP23008(0x20)
+        self.buttons = mcs.MCP23008(i2c_address)
         for i in range(8):
             self.buttons.setup(i, GPIO.IN)
             self.buttons.pullup(i, True)
+
+    def add_callback(self, callback):
+        # Setup interupt
+        gpio = GPIO.get_platform_gpio()
+      
+        gpio.setup(interupt_pin, GPIO.IN)
+        gpio.add_event_detect(interupt_pin, GPIO.FALLING, callback=callback, bouncetime=5)
+        while True:
+            pass
+         
 
     def get_buttons(self):
         return self.buttons.input_pins(range(8))
@@ -17,6 +30,7 @@ class Buttons(object):
 if __name__ == "__main__":
     butts = Buttons()
     print(butts.get_buttons())
+    butts.add_callback(lambda x: print(butts.get_buttons()))
 
 
 
