cf583925f0e93017146c6f9b13c8f157fbd0cbfe
John Cooper <john@choffee.co.uk>
2016-08-21 18:05:03 +0100

    Add pygame emulator for no pi

---
+++ bitcolors.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Wrapper for making the lights run.
+colors = [
+    [0, 0, 0],          # 0 - Black
+    [255, 255, 255],    # 1 - White
+    [255, 0, 0],        # 2 - Red
+    [0, 255, 0],        # 3 - Green
+    [0, 0, 255],        # 4 - Blue
+    [255, 128, 0],      # 5 - Orange
+    [255, 0, 255],      # 6 - Purple
+    [0, 255, 255],      # 7 - Cyan
+]
--- nolights.py
+++ nolights.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
+import pygame
+from bitcolors import colors
+
+MAX_X=424
+MAX_Y=600
+BLOCK_SIZE=20
+
+def make_square(x, y):
+    b_size = BLOCK_SIZE
+    return (x * b_size + 1, MAX_Y - (y + 1) * b_size - 2, b_size, b_size)
+
+def make_color(num):
+    return colors[num]
 
 class Board(object):
     def __init__(self):
-        pass
-    def show_board(self, nothing):
-        pass
+        self.screen = pygame.display.set_mode((MAX_X, MAX_Y))
+        top = MAX_Y - ( 20 * BLOCK_SIZE) - 3
+        pygame.draw.rect(self.screen, (255, 255, 255), (0, top, 10 * BLOCK_SIZE + 2, BLOCK_SIZE * 20 + 2), 1)
+
+    def show_board(self, board):
+        for line_no, line in enumerate(board):
+            for cell_no, cell in enumerate(line):
+                print(make_square(line_no, cell_no))
+                pygame.draw.rect(self.screen, make_color(cell), make_square(cell_no, line_no))
+
+        pygame.draw.rect(self.screen, (255, 255, 255), (0, 0, 10, 10))
+        pygame.display.update()
